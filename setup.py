65945f9ba6d147ab066a71fc420c72417e1f4dd8
Simeon Borko <simeon.borko@gmail.com>
2018-07-31 22:49:08 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name='SlovakEuroPronouncer', version='1.0',
+      packages=['SlovakEuroPronouncer'],
+      package_dir={'SlovakEuroPronouncer': 'src/SlovakEuroPronouncer'})
